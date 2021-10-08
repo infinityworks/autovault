3b51c4093f9a7ffe3f7ab0bc9a5eef3e40b95873
Adam Dewberry <38838996+AdamDewberry@users.noreply.github.com>
2021-10-08 14:42:54 +0100

    - Create landing table DDL statement sql exporter. This will find all metadata files in the source_metadata directory and produce `.sql` files. - Include sample JSON metadata schema to run.

--- generate_raw_vault/app/load_metadata.py
+++ generate_raw_vault/app/load_metadata.py
@@ -19,46 +19,10 @@ def get_sats_from_hub(business_topics, hub):
     sats = {hub : [topic.get("business_definition") for topic in topics]}
     return sats
 
-
-def export_source_ddl(ddl, source_name):
-    formatted_source_name = source_name.lower()
-    with open(f"./source_tables/DDL/{formatted_source_name}.sql", "w") as sql_export:
-        sql_export.write(ddl)
-
-def create_source_table_ddl(metadata, database, schema):
-    source_name = metadata.get("source_name")
-    business_keys = get_source_business_keys(metadata)
-    payload = [f'''{list(business_key.keys())[0]} {list(business_key.values())[0]}''' for business_key in business_keys]
-    payload_columns = f",\n{4*chr(32)}".join(payload)
-    ddl = f'''CREATE TABLE IF NOT EXISTS {database}.{schema}.{source_name} (
-    {payload_columns},
-    RECORD_SOURCE STRING, 
-    LOAD_DATETIME TIMESTAMP_TZ
-    )
-    '''
-    return ddl
-
-
-def get_source_business_keys(metadata):
-    topics = [topic for topic in metadata.get("business_topics").values()]
-    source_keys = format_business_key_list(topics)
-    return source_keys
-
-def format_business_key_list(source_topics):
-    source_keys = [hub_info.get("business_keys") for hub_info in source_topics]
-    return source_keys
-
-# get_source_payload columns, similar to above
-# may be useful -> list(itertools.chain(*source_keys))
-
 if __name__ == '__main__':
     metadata = load_metadata_file('./source_metadata/customers.json')
     business_topics = get_business_topics(metadata)
     
     hubs = get_hubs_from_business_topics(business_topics)
 
-    source_business_keys = get_source_business_keys(metadata)
-    ddl  = create_source_table_ddl(metadata, database="AUTOVAULT", schema="PUBLIC")
-    print(ddl)
-    export_source_ddl(ddl, source_name= "CUSTOMERS")
     
---
+++ generate_raw_vault/app/main.py
@@ -0,0 +1,15 @@
+import json
+import itertools
+from generate_raw_vault.app.export_ddl_statement import export_all_ddl_statments
+
+def main():
+    export_all_ddl_statments()
+    # create_stages()
+    # create_hubs()
+    # create_sats()
+    # create_links()
+
+if __name__ == '__main__':
+    main()
+
+    
