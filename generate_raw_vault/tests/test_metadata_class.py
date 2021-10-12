c5455379db5d01276780aa11eed7559e4c5ba743
kiranbhat <kiran.bhat@infinityworks.com>
2021-10-12 17:24:45 +0100

    added tests for methods get_versioned_source_name and get_source_business_topics

--- generate_raw_vault/tests/test_metadata_class.py
+++ generate_raw_vault/tests/test_metadata_class.py
@@ -16,3 +16,36 @@ class TestMetadata:
         test_target_database = test_metadata.get_target_database()
         expected_target_database = "AUTOVAULT"
         assert test_target_database == expected_target_database
+
+    @pytest.mark.usefixtures("sample_metadata")
+    def test_get_versioned_source_name(self, sample_metadata):
+        test_metadata = Metadata(sample_metadata)
+        test_target_version = test_metadata.get_versioned_source_name()
+        expected_target_version = "TEST_V1"
+        assert test_target_version == expected_target_version
+
+    @pytest.mark.usefixtures("sample_metadata")
+    def test_get_source_business_topics(self, sample_metadata):
+        test_metadata = Metadata(sample_metadata)
+        test_target_source_business_topics = test_metadata.get_source_business_topics()
+        expected_target_source_business_topics = [
+            {
+                "business_keys": {"pk1": "STRING"},
+                "business_attributes": [
+                    {
+                        "business_definition": "SAT1",
+                        "payload": {"sat1_col1": "STRING", "sat1_col2": "STRING"},
+                    },
+                    {"business_definition": "SAT2", "payload": {"sat2_col1": "STRING"}},
+                ],
+            },
+            {
+                "business_keys": {"pk2": "STRING"},
+                "business_attributes": [
+                    {"business_definition": "SAT3", "payload": {"sat3_col1": "STRING"}}
+                ],
+            },
+        ]
+        assert (
+            test_target_source_business_topics == expected_target_source_business_topics
+        )
