be3f4b30b443e25c2cf9555ddfba0bb419e75d87
Adam Dewberry <38838996+AdamDewberry@users.noreply.github.com>
2021-12-15 13:22:54 +0000

    Generate raw vault model

---
+++ models/raw_vault/hubs/product_hub.sql
@@ -0,0 +1,18 @@
+{{ config(
+  materialized='incremental',
+  schema = "HUBS",
+  alias = "PRODUCT"
+  ) }}
+
+{%- set source_model = ["stg_products_v1",
+                        "stg_transactions_v1"] -%}
+{%- set src_pk = "PRODUCT_HK" -%}
+{%- set src_nk = "PRODUCT_ID" -%}
+{%- set src_ldts = "LOAD_DATETIME" -%}
+{%- set src_source = "RECORD_SOURCE" -%}
+
+{{ dbtvault.hub(src_pk=src_pk,
+                src_nk=src_nk,
+                src_ldts=src_ldts,
+                src_source=src_source,
+                source_model=source_model) }}
