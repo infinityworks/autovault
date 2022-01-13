3333cb0744a045a684e897562f07d390b1aca72c
Adam Dewberry <38838996+AdamDewberry@users.noreply.github.com>
2022-01-13 17:23:27 +0000

    Continue rebase

--- generate_raw_vault/app/create_model_directories.py
+++ generate_raw_vault/app/create_model_directories.py
@@ -1,21 +1,12 @@
 from pathlib import Path
 
-model_directories = [
-    "source_metadata",
-    "./source_tables/DDL",
-    "./models/raw_vault/hubs",
-    "./models/raw_vault/links",
-    "./models/raw_vault/sats",
-    "./models/raw_vault/stages",
-]
-
 
 def create_model_directories(model_directories):
     for directory in model_directories:
         path = Path(directory)
         path.mkdir(parents=True, exist_ok=True)
-        gitkeeps = path / ".gitkeep"
-        gitkeeps.touch(exist_ok=True)
+        gitkeep = path / ".gitkeep"
+        gitkeep.touch(exist_ok=True)
 
 
 if __name__ == "__main__":
