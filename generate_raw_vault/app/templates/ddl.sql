ab29f21505fcf456fcfe49e4e2448fdf0e4d0e71
Adam Dewberry <38838996+AdamDewberry@users.noreply.github.com>
2022-12-28 14:57:30 +0000

    Modify source table DDL generation to 'CREATE IF NOT EXISTS' to avoid deployment errors in repeat CI runs  (#74)
    * Modify source table DDL generation to 'CREATE IF NOT EXISTS' to avoid deployment errors now that SELECT table GRANTS have been added to each source .sql file. This assumes that the files are run through a CI pipeline whenever there is a file change, e.g. if a new access role is added to the source metadata JSON document it should run in the pipeline and execute the new SELECT statement
    
    * Make the role name in the table grant case sensitive
     (#74)

--- generate_raw_vault/app/templates/ddl.sql
+++ generate_raw_vault/app/templates/ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "$target_database"."$target_schema"."$versioned_source_name" (
+CREATE TABLE IF NOT EXISTS "$target_database"."$target_schema"."$versioned_source_name" (
     $keys_and_types_str,
     $payload_columns_and_types_str,
     "RECORD_SOURCE" STRING,
